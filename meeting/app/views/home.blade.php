cdf399fa887058a1a67e83fe7df52a155e84ae70
Carlos Ernesto Saenz Vega <cesaenzv@hotmail.com>
2013-10-24 16:16:34 -0500

    Views Blade

---
+++ meeting/app/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('shared.master')
+
+@section('Content')
+	<div class="col-md-12">
+		BIENVENIDO A LA PAGINA DE INICIO
+		{{Form::open()}}
+	</div>
+@stop
---
+++ meeting/app/views/user/index.blade.php
@@ -0,0 +1,7 @@
+@extends('shared.master')
+
+@section('Content')
+	<div class="col-md-12">
+		ESTA ES LA PAGINA INICIAL DEL USUARIO		
+	</div>
+@stop
